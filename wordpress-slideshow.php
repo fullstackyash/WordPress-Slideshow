7f74529834b03ea6c4aa525d3aac1b79ba85bd9c
Yash Chopra <yash.chopra@infobeans.com>
2022-10-30 21:17:07 +0530

    add scaffold plugin

---
+++ wordpress-slideshow.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Plugin Name:     Wordpress Slideshow
+ * Plugin URI:      https://github.com/fullstackyash/WordPress-Slideshow
+ * Description:     Wordpress plugin for creating slideshow.
+ * Author:          Yash Chopra
+ * Author URI:      https://github.com/fullstackyash
+ * Text Domain:     wordpress-slideshow
+ * Domain Path:     /languages
+ * Version:         0.1.0
+ *
+ * @package         Wordpress_Slideshow
+ */
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit; // Exit if accessed directly.
+}
